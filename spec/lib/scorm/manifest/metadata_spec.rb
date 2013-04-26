cb27d268d7dc59087dfdfb3c2b23db71b5640bca
Per Christian B. Viken <perchr@northblue.org>
2013-04-26 10:03:40 +0200

    Convenience to avoid repeating the parsing steps

--- spec/lib/scorm/manifest/metadata_spec.rb
+++ spec/lib/scorm/manifest/metadata_spec.rb
@@ -4,7 +4,7 @@ require 'scorm/manifest'
 describe Scorm::Manifest::Metadata do
   describe ".from_xml" do
     it "reads the important bits" do
-      doc = Nokogiri::XML(scorm_manifest("version_scorm_2004_4th"), "utf-8") { |config| config.nonet }
+      doc = xml_scorm_manifest("version_scorm_2004_4th")
       metadata = Scorm::Manifest::Metadata.from_xml(doc.xpath("/xmlns:manifest/xmlns:metadata"))
       metadata.schema.should eq("ADL SCORM")
       metadata.schemaversion.should eq("2004 4th Edition")
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,7 @@ def scorm_manifest(name)
     File.join(__FILE__, "..", "fixtures", "manifests", "#{name}.xml")
   )
 end
+
+def xml_scorm_manifest(name)
+  Nokogiri::XML(scorm_manifest(name), "utf-8") { |config| config.nonet }
+end
