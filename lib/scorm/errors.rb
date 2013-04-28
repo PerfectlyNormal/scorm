3197c3d3c66f16cc190b5de38c5eea13cdb1dd48
Per Christian B. Viken <perchr@northblue.org>
2013-04-28 19:59:56 +0200

    Add RequiredItemMissing and DuplicateItem exceptions.
    NoMetadataError/NoOrganizationsError and their related "too-many" exceptions is too specific,
    and we would like to avoid adding those kinds of exceptions every time the standard requires
    one, and only one, of some kind of element.
    
    Therefore, introduce the RequiredItemMissing and DuplicateItem, and have the more specific inherit from these

--- lib/scorm/errors.rb
+++ lib/scorm/errors.rb
@@ -1,10 +1,14 @@
 module Scorm
   module Errors
     class InvalidManifest         < RuntimeError; end
-    class NoMetadataError         < InvalidManifest; end
-    class DuplicateMetadataError  < InvalidManifest; end
-    class NoOrganizationsError    < InvalidManifest; end
-    class DuplicateOrganizationsError < InvalidManifest; end
+    class RequiredItemMissing     < InvalidManifest; end
+    class DuplicateItem           < InvalidManifest; end
+
+    class NoMetadataError         < RequiredItemMissing; end
+    class DuplicateMetadataError  < DuplicateItem; end
+    class NoOrganizationsError    < RequiredItemMissing; end
+    class DuplicateOrganizationsError < DuplicateItem; end
+
     class UnsupportedSCORMVersion < RuntimeError; end
     class InvalidSCORMVersion     < InvalidManifest; end
   end
